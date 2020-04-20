1cf4b72b1ed3088d66e22d3b291aa95843801ca2
ximing <ximing>
2020-04-20 15:17:57 +0000

    Deploying to gh-pages from  @ d3ef30205e8f8cd64ab0192bde7ff53e37076808 🚀

---
+++ component---src-pages-404-tsx-d941283a37f65cc5104f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(n,t,e){"use strict";e.r(t);e("q1tI");var c=e("Wbzz"),o=e("NqE+"),u=e("TBFr"),a=e("+ego"),l=e("aD51");t.default=function(){return Object(l.d)(a.a,null,Object(l.d)(o.a,null,Object(l.d)(u.a,null,Object(l.d)("h1",null,"404: Page not found."),Object(l.d)("p",null,Object(l.d)(c.a,{to:"/"},"Go back home")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-d941283a37f65cc5104f.js.map
