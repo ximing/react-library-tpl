1ed48fbc09bbb8f0b63497bd003625cddb7573a5
ximing <ximing>
2020-05-09 13:51:50 +0000

    Deploying to gh-pages from  @ 75775f6cafc6d197b9c7c5b250504bb744d431ac 🚀

---
+++ component---src-pages-page-2-tsx-049343ac279b7d1496aa.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"+xhR":function(e,l,t){"use strict";t.r(l);t("q1tI");var n=t("Wbzz"),c=t("NqE+"),u=t("TBFr"),a=t("+ego"),o=t("aD51");l.default=function(){return Object(o.d)(a.a,null,Object(o.d)(c.a,null,Object(o.d)(u.a,null,Object(o.d)("h1",null,"Hi from the second page"),Object(o.d)("p",null,"Welcome to page 2"),Object(o.d)("ul",null,Object(o.d)("li",null,Object(o.d)(n.a,{to:"/"},"Take me back home."))))))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-049343ac279b7d1496aa.js.map
